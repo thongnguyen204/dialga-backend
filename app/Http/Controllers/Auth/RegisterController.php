57f13b791c35f1cd0744d5ad6f0e65832aee36b0
Thong Nguyen <dev.thongnguyen@gmail.com>
2022-05-24 16:29:56 +0700

    welcome mail

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Mail\Welcome;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -18,6 +19,9 @@ class RegisterController extends Controller
             'name'  => $request->name,
             'password'  => Hash::make($request->password)
         ]);
+
+        if($user)
+            Mail::to($request->email)->send(new Welcome());
 
         return response()->json([
             'name'  => $request->name,
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,7 @@
+@component('mail::message')
+
+Welcome to our world !!!
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
